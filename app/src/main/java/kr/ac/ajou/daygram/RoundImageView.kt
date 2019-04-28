e32a6fb820bf7cc2a5350085ce4b4031cab18844
parkhaneul <gksmf077080@gmail.com>
2019-04-28 16:17:47 +0900

    2019_04_28 - 4

--- app/src/main/java/kr/ac/ajou/daygram/RoundImageView.kt
+++ app/src/main/java/kr/ac/ajou/daygram/RoundImageView.kt
@@ -2,21 +2,18 @@ package kr.ac.ajou.daygram
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.widget.ImageView
 
 class RoundImageView : ImageView{
-    var radius : Float = 100.0f
-
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     override fun onDraw(canvas: Canvas){
-        //var drawable : Drawable = context.getDrawable(R.drawable.round_box)
-        //this.background = drawable;
-        //this.clipToOutline = true
     }
 }
